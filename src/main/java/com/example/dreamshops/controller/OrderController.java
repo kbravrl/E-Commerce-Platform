fd79fda109eb54f410aa05e67cb8a7ae4e54f242
Kübra Varol <kubravarol499@gmail.com>
2025-06-15 23:05:56 +0300

    add order and orderitem model/repository/service/controller/dto/enum files

---
+++ src/main/java/com/example/dreamshops/controller/OrderController.java
@@ -0,0 +1,51 @@
+package com.example.dreamshops.controller;
+
+import com.example.dreamshops.dto.OrderDto;
+import com.example.dreamshops.exceptions.ResourceNotFoundException;
+import com.example.dreamshops.response.ApiResponse;
+import com.example.dreamshops.service.order.IOrderService;
+import com.example.dreamshops.model.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("${api.prefix}/orders")
+public class OrderController {
+    private final IOrderService orderService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse> createOrder(@RequestParam Long userId) {
+        try {
+            Order order = orderService.placeOrder(userId);
+            OrderDto orderDto = orderService.convertToDto(order);
+            return ResponseEntity.ok(new ApiResponse("Order created successfully", orderDto));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse("Failed to create order: " + e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/order/{orderId}")
+    public ResponseEntity<ApiResponse> getOrderById(@PathVariable Long orderId) {
+        try {
+            OrderDto order = orderService.getOrderById(orderId);
+            return ResponseEntity.ok(new ApiResponse("Order retrieved successfully", order));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse("Order not found: " + e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<ApiResponse> getUserOrders(@PathVariable Long userId) {
+        try {
+            List<OrderDto> orders = orderService.getUserOrders(userId);
+            return ResponseEntity.ok(new ApiResponse("User orders retrieved successfully", orders));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse("No orders found for user: " + e.getMessage(), null));
+        }
+    }
+}
---
+++ src/main/java/com/example/dreamshops/service/order/OrderService.java
@@ -0,0 +1,92 @@
+package com.example.dreamshops.service.order;
+
+import com.example.dreamshops.dto.OrderDto;
+import com.example.dreamshops.enums.OrderStatus;
+import com.example.dreamshops.exceptions.ResourceNotFoundException;
+import com.example.dreamshops.model.Cart;
+import com.example.dreamshops.model.Order;
+import com.example.dreamshops.model.OrderItem;
+import com.example.dreamshops.model.Product;
+import com.example.dreamshops.repository.OrderRepository;
+import com.example.dreamshops.repository.ProductRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import com.example.dreamshops.service.cart.CartService;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService implements IOrderService {
+    private final OrderRepository orderRepository;
+    private final CartService cartService;
+    private final ModelMapper mabelMapper;
+    private final ProductRepository productRepository;
+
+    @Transactional
+    @Override
+    public Order placeOrder(Long userId) {
+        Cart cart = cartService.getCartByUserId(userId);
+        Order order = createOrder(cart);
+        List<OrderItem> orderItems = createOrderItems(order, cart);
+        order.setOrderItems(new HashSet<>(orderItems));
+        order.setTotalAmount(calculateTotalAmount(orderItems));
+        Order savedOrder = orderRepository.save(order);
+        cartService.clearCart(cart.getId());
+        return savedOrder;
+
+    }
+
+    private Order createOrder(Cart cart) {
+        Order order = new Order();
+        order.setUser(cart.getUser());
+        order.setOrderStatus(OrderStatus.PENDING);
+        order.setOrderDate(LocalDate.now());
+        return order;
+    }
+
+    private List<OrderItem> createOrderItems(Order order, Cart cart) {
+        return cart.getItems().stream().map(cartItem -> {
+            Product product = cartItem.getProduct();
+            product.setInventory(product.getInventory() - cartItem.getQuantity());
+            productRepository.save(product);
+            return new OrderItem(
+                    order,
+                    product,
+                    cartItem.getQuantity(),
+                    cartItem.getUnitPrice());
+        }).toList();
+
+    }
+
+    private BigDecimal calculateTotalAmount(List<OrderItem> orderItems) {
+        return orderItems.stream()
+                .map(item -> item.getPrice()
+                        .multiply(new BigDecimal(item.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    @Override
+    public OrderDto getOrderById(Long orderId) {
+        return orderRepository.findById(orderId)
+                .map(this::convertToDto)
+                .orElseThrow(() -> new ResourceNotFoundException("Order not found with id: " + orderId));
+    }
+
+
+    @Override
+    public List<OrderDto> getUserOrders(Long userId) {
+        List<Order> orders = orderRepository.findByUserId(userId);
+        return orders.stream().map(this:: convertToDto).toList();
+    }
+
+    @Override
+    public OrderDto convertToDto(Order order) {
+        return mabelMapper.map(order, OrderDto.class);
+    }
+}
