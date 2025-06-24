30dd971ae4f45a74aa5c5bcf6466fd8c113db7c3
Kübra Varol <kubravarol499@gmail.com>
2025-06-24 18:06:37 +0300

    Initializes client-side React application
    Sets up the basic structure for a React application using Vite, including:
    - Initial React components and configurations
    - Adds necessary dependencies like React Router, Bootstrap, and Axios
    - Configures ESLint with React hooks and refresh support
    - Adds .gitignore file for client-side node_modules and vscode files

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "./pages/login/Login";
+import Product from "./pages/product/Product";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
