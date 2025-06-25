b5d32b7acc5a9a334d75bbeb6218ddf897856844
Kübra Varol <kubravarol499@gmail.com>
2025-06-25 17:52:14 +0300

    Updates login page UI and text content
    Improves the user interface of the login page.
    
    Replaces English text with Turkish for a better user
    experience for the target audience.
    
    Updates button to redirect to the products page
    after a user has logged in.

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,54 +1,46 @@
-import React from "react";
-import './Login.css';
-import logo from '../../assets/cart.svg';
+import "./Login.css";
+import logo from "../../assets/cart.svg";
 
 const Login = () => {
   return (
     <div>
       <form class="form-signin">
         <div class="text-center mb-4">
-          <img
-            class="mb-4"
-            src={logo}
-            alt={logo}
-            width="72"
-            height="72"
-          />
+          <img class="mb-4" src={logo} alt={logo} width="72" height="72" />
 
-          <h1 class="h3 mb-3 font-weight-normal">Floating labels</h1>
-          <p>
-            Build form controls with floating labels via the{" "}
-            
-          </p>
+          <h1 class="h3 mb-3 font-weight-normal">Hoşgeldiniz</h1>
+          <p>Alışveriş bir tık öte — stil seninle başlar.</p>
         </div>
 
         <div class="form-label-group mb-2">
-        <label for="inputEmail">Email address</label>
+          <label for="inputEmail">Email Adresi</label>
           <input
             type="email"
             id="inputEmail"
             class="form-control"
-            placeholder="Email address"
+            placeholder="Email adresi"
             required=""
             autofocus=""
           />
-          
         </div>
 
         <div class="form-label-group mb-2">
-        <label for="inputPassword">Password</label>
+          <label for="inputPassword">Şifre</label>
           <input
             type="password"
             id="inputPassword"
             class="form-control"
-            placeholder="Password"
+            placeholder="Şifre"
             required=""
           />
-          
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">
-          Sign in
-        </button>
+        <a
+          class="btn btn-primary"
+          href="http://localhost:5173/products"
+          role="button"
+        >
+          Giriş Yap
+        </a>
       </form>
     </div>
   );
