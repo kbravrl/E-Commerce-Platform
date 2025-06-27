7a70c75224f9b93b0de426c047d7e461b5f310f7
Kübra Varol <kubravarol499@gmail.com>
2025-06-27 16:35:09 +0300

    Adds product list by category
    Implements a product list page that fetches and displays
    products based on the selected category.
    
    Retrieves the category name from the route parameters
    to fetch corresponding products from the backend.
    Displays fetched products in a grid layout.

---
+++ client/src/pages/product/ProductList.jsx
@@ -0,0 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "..//../components/Navbar";
+import Card from "../../components/Card";
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+const ProductList = () => {
+  const { categoryName } = useParams();
+  const [productsByCategory, setProductsByCategory] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/products/by-category/${categoryName}`)
+      .then((response) => {
+        setProductsByCategory(response.data.data);
+      })
+      .catch((error) => console.error(error));
+
+  }, [categoryName]);
+
+  return (
+    <>
+      <Navbar />
+      <div className="container py-5">
+        <h2 className="text-center mb-5">{categoryName}</h2>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+          {productsByCategory.map((product) => (
+            <Card key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductList;
