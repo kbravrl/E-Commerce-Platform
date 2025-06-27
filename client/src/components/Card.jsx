4a286491963dbb9283cf04cd4bc69ba50efda4ef
Kübra Varol <kubravarol499@gmail.com>
2025-06-28 02:17:49 +0300

    Adds functionality to add products to cart
    Implements the functionality to add products to the user's cart.
    Sends a POST request to the backend to add the product
    and displays a success message to the user.

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,5 +1,30 @@
-const baseUrl = import.meta.env.VITE_API_BASE_URL
+import axios from "axios";
+
 const Cart = ({ product }) => {
+  const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+  const handleAddToCart = () => {
+    const token = localStorage.getItem("token");
+
+    axios
+      .post(
+        `${baseUrl}/cartItems/add?productId=${product.id}&quantity=${1}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          withCredentials : true
+        }
+      )
+      .then(() => {
+        alert("Product added to cart");
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
+  };
+
   return (
     <>
       <div className="col" key={product.id}>
@@ -18,7 +43,7 @@ const Cart = ({ product }) => {
             <p className="card-text">{product.description}</p>
             <div className="d-flex justify-content-between align-items-center">
               <span className="h5 mb-0">{product.price}$</span>
-              <button className="btn btn-outline-primary">
+              <button className="btn btn-outline-primary" onClick={handleAddToCart}>
                 <i className="bi bi-cart-plus"></i> Add to cart
               </button>
             </div>
