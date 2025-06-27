5cdcb2a05755e955d6bac8be85a7883d52ce538e
Kübra Varol <kubravarol499@gmail.com>
2025-06-27 16:34:25 +0300

    Fetches and displays products
    Implements the product listing page.
    
    Fetches product data from the API and displays it in a responsive grid.
    Uses a Card component to display each product's information.

--- client/src/pages/product/Product.jsx
+++ client/src/pages/product/Product.jsx
@@ -1,95 +1,33 @@
-import React from "react";
-import logo from "../../assets/cart2.png";
-import person from "../../assets/person.png"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Navbar from "../../components/Navbar";
+import Card from "../../components/Card";
+import "./Product.css";
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL
 
 const Product = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/products/all`)
+      .then(response => { 
+        setProducts(response.data.data);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
   return (
-    <div>
-      <nav
-        class="navbar navbar-expand-md bg-dark sticky-top border-bottom"
-        data-bs-theme="dark"
-      >
-        {" "}
-        <div class="container">
-          {" "}
-          <a class="navbar-brand d-md-none" href="#">
-            {" "}
-            Aperture
-          </a>{" "}
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvas"
-            aria-controls="offcanvas"
-            aria-label="Toggle navigation"
-          >
-            {" "}
-            <span class="navbar-toggler-icon"></span>{" "}
-          </button>{" "}
-          <div
-            class="offcanvas offcanvas-end"
-            tabindex="-1"
-            id="offcanvas"
-            aria-labelledby="offcanvasLabel"
-          >
-            {" "}
-            <div class="offcanvas-header">
-              {" "}
-              <h5 class="offcanvas-title" id="offcanvasLabel">
-                Aperture
-              </h5>{" "}
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="offcanvas"
-                aria-label="Close"
-              ></button>{" "}
-            </div>{" "}
-            <div class="offcanvas-body">
-              {" "}
-              <ul class="navbar-nav flex-grow-1 justify-content-between">
-                {" "}
-                <li class="nav-item">
-                  <a class="nav-link" href="#" aria-label="Aperture">
-                    {" "}
-                    <svg class="bi" width="24" height="24" aria-hidden="true">
-                      <use xlink:href="#aperture"></use>
-                    </svg>{" "}
-                  </a>
-                </li>{" "}
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Products
-                  </a>
-                </li>{" "}
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Categories
-                  </a>
-                </li>{" "}
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                    Orders
-                  </a>
-                </li>{" "}
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                  <img src={logo} alt={logo} width="25" height="25" style={{ marginRight: '10px' }} />
-                    Cart
-                  </a>
-                </li>{" "}
-                <li class="nav-item">
-                  <a class="nav-link" href="#" aria-label="Cart">
-                    <img src={person} alt={person} width="25" height="25"/>
-                  </a>
-                </li>{" "}
-              </ul>{" "}
-            </div>{" "}
-          </div>{" "}
-        </div>{" "}
-      </nav>
-    </div>
+    <>
+      <Navbar />
+      <div className="container py-5">
+        <h2 className="text-center mb-5">Our Products</h2>
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+          {products.map(product => (<Card key={product.id} product={product}/>))}
+        </div>
+      </div>
+    </>
   );
 };
 
