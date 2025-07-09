82a9ea9748d475a14f69b2ee60673992038ece9d
Kübra Varol <kubravarol499@gmail.com>
2025-07-09 23:45:24 +0300

    Add UserDelete component to User Page

---
+++ client/src/components/UserDelete.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+
+const UserDelete = () => {
+  const token = localStorage.getItem("token");
+
+  const deleteAccount = () => {
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete your account?"
+    );
+    if (confirmDelete) {
+      axios
+        .delete(`${baseUrl}/users/delete`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then(() => {
+          alert("Account deleted successfully.");
+          localStorage.removeItem("token");
+          window.location.href = "/";
+        })
+        .catch((err) => {
+          console.error("Error deleting account:", err);
+          alert("Failed to delete account.");
+        });
+    }
+  };
+
+  return (
+    <div className="mb-3">
+      <h5>Delete Account</h5>
+      <p>You are about to delete your account.</p>
+      <button type="submit" className="btn btn-dark" onClick={deleteAccount}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default UserDelete;
