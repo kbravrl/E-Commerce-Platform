b5bd3f7696b99d8ecaaaaffe364bda1ef72c1b06
Kübra Varol <kubravarol499@gmail.com>
2025-07-11 22:56:01 +0300

    Solve the try catch bloks problem

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import LogoHeader from "../../components/LogoHeader"; 
+import LogoHeader from "../../components/LogoHeader";
 import InputField from "../../components/InputField";
 import "./Login.css";
 
@@ -15,25 +15,23 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (!email || !password) {
-      alert("All fields are required!");
-    }
-
     try {
-      axios
-        .post(`${baseUrl}/auth/login`, {
-          email,
-          password,
-        })
-        .then((response) => {
-          const token = response.data.data.token;
-          localStorage.setItem("token", token);
-        });
+      const response = await axios.post(`${baseUrl}/auth/login`, {
+        email,
+        password,
+      });
 
+      const token = response.data.data.token;
+      localStorage.setItem("token", token);
       navigate("/products");
     } catch (error) {
       if (error.response && error.response.status === 401) {
         alert("Incorrect email or password.");
+      } else {
+        alert(
+          "An unexpected error occurred: " +
+            (error.response?.data?.message || error.message)
+        );
       }
     }
   };
@@ -42,8 +40,20 @@ const Login = () => {
     <div>
       <form className="form-signin" onSubmit={handleSubmit}>
         <LogoHeader />
-        <InputField id={"inputEmail"} label={"Email Address"} type={"email"} placeholder={"username"} onChange={(e) => setEmail(e.target.value)}/>
-        <InputField id={"inputPassword"} label={"Password"} type={"password"} placeholder={"passport"} onChange={(e) => setPassword(e.target.value)}/>
+        <InputField
+          id={"inputEmail"}
+          label={"Email Address"}
+          type={"email"}
+          placeholder={"username"}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <InputField
+          id={"inputPassword"}
+          label={"Password"}
+          type={"password"}
+          placeholder={"passport"}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit" className="btn btn-primary w-100 mt-3">
           Login
         </button>
