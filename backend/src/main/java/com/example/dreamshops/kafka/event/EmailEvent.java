1137264259e8edf005ec31012662229aec174477
Kübra Varol <kubravarol499@gmail.com>
2025-07-26 19:40:40 +0300

    Add EmailEvent

---
+++ backend/src/main/java/com/example/dreamshops/kafka/event/EmailEvent.java
@@ -0,0 +1,15 @@
+package com.example.dreamshops.kafka.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailEvent {
+    private String to;
+    private String subject;
+    private String body;
+    private String eventType;
+}
